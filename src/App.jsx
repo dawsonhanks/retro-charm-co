8ce862ed6e91b0fff68dc77e9c5ba2643eb52b6a
Dawson Hanks <dawsonhanks@Dawsons-MacBook-Pro.local>
2026-05-07 22:31:08 -0600

    Fix blank tab pages by simplifying outlet rendering and remove probe

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
-import { Routes, Route, useLocation, Outlet } from 'react-router-dom'
-import { AnimatePresence, motion } from 'framer-motion'
+import { Routes, Route, Outlet } from 'react-router-dom'
 import { Navbar } from './components/Navbar.jsx'
 import { Footer } from './components/Footer.jsx'
 import Home from './pages/Home.jsx'
@@ -8,30 +7,13 @@ import Gallery from './pages/Gallery.jsx'
 import FindUs from './pages/FindUs.jsx'
 import About from './pages/About.jsx'
 
-function AnimatedOutlet() {
-  const location = useLocation()
-  return (
-    <AnimatePresence mode="wait">
-      <motion.main
-        key={location.pathname}
-        initial={{ opacity: 0, y: 14 }}
-        animate={{ opacity: 1, y: 0 }}
-        exit={{ opacity: 0, y: -12 }}
-        transition={{ duration: 0.32, ease: [0.22, 1, 0.36, 1] }}
-        className="flex-1"
-        id="main-content"
-      >
-        <Outlet />
-      </motion.main>
-    </AnimatePresence>
-  )
-}
-
 function Shell() {
   return (
     <div className="flex min-h-dvh flex-col bg-jscolors-cream">
       <Navbar />
-      <AnimatedOutlet />
+      <main className="flex-1" id="main-content">
+        <Outlet />
+      </main>
       <Footer />
     </div>
   )
