1b88840b98193290080ef6f5409044aa9307f62f
Dawson Hanks <dawsonhanks@Dawsons-MacBook-Pro.local>
2026-08-17 06:59:58 -0600

    Add Back-to-School sale banner

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { Analytics } from '@vercel/analytics/react'
 import { Navbar } from './components/Navbar.jsx'
 import { Footer } from './components/Footer.jsx'
 import { NextMarketBanner } from './components/NextMarketBanner'
+import { SaleBanner } from './components/SaleBanner.jsx'
 import { ScrollToTop } from './components/ScrollToTop.jsx'
 import { AnalyticsBootstrap } from './components/AnalyticsBootstrap.jsx'
 import Home from './pages/Home.jsx'
@@ -62,6 +63,7 @@ function Shell() {
       >
         Skip to main content
       </a>
+      <SaleBanner />
       <Navbar />
       {showMarketBanner ? <NextMarketBanner /> : null}
       <main className="min-w-0 flex-1" id="main-content" tabIndex={-1}>
